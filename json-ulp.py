8c9400bee4fc5c4c912c82998798f587e4d3fcb7
Alex Ray <ajray@us.ibm.com>
2011-11-04 15:55:31 -0400

    changing the printery things to just go to stdout for now

--- json-ulp.py
+++ json-ulp.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-# json-ulp.py - generate json.ulp, Eagle CAD program to export circuits to JSON
+# json-ulp.py - generate json.ulp, which exports Eagle to JSON
+# http://github.com/ajray/schematic-file-converter
 # Alex Ray (2011) <ajray@ncsu.edu>
 
-# TODO(ajray): use pluralization instead of rewriting ('libraries','library')
-#             figure out what's up with clearance[number]
 # TODO do comma's properly
 import json
 eagle = json.loads(open("eagle511.json").read())
@@ -15,10 +14,8 @@ header = """
 // Alex Ray (2011) <ajray@ncsu.edu>
 """
 misc = """
-string cn = ",\n"; // comma and newline
-string bn = "{\n"; // brace and newline
 string esc(string s) { // JSON string escapes
-    string out = "";
+    string out = "\""; // open quote
     for (int i = 0; s[i]; ++i) {
         switch(s[i]) {
             case '"': out += "\\\""; break;
@@ -32,36 +29,33 @@ string esc(string s) { // JSON string escapes
             default: out += s[i];
         }
     }
+    out += "\""; // close quote
     return out;
 }
-string beg(string a) { // pair with the start of an object
-    string s;
-    sprintf(s,"\"%s\": ",esc(a));
-    return s;
+void po(string a) {           // pair with the start of an object
+    printf("\"%s\": { ",a);
 }
-string pl(string a) { // pair with the start of a list
-    string s;
-    sprintf(s,"\"%s\": [\n",esc(a));
-    return s;
+void poo() {                  // end of an object
+    printf("}");
 }
-string ps(string a, string b) { // pair with string
-    string s;
-    sprintf(s,"\"%s\":\"%s\"",esc(a),esc(b));
-    return s;
+void pl(string a) {           // pair with the start of a list
+    printf("\"%s\": [ ",a);
 }
-string pi(string a, int b) { // pair with int
-    string s;
-    sprintf(s,"\"%s\":%d",esc(a),b);
-    return s;
+void pll() {                  // end of a list
+    printf("]");
 }
-string pr(string a, real b) { // pair with real
-    string s;
-    sprintf(s,"\"%s\":%g",esc(a),b);
-    return s;
+void ps(string a, string b) { // pair with string
+    printf("\"%s\": %s ",a,esc(b));
+}
+void pi(string a, int b) {    // pair with int
+    printf("\"%s\": %d ",a,b);
+}
+void pr(string a, real b) {   // pair with real
+    printf("\"%s\": %g ",esc(a),b);
 }
 """
 
-def makepick(l):
+def makepick(l): # TODO: support bitmasks as well as enums
   """ Make a pick function from a list """
   name = l[0]
   s = "string pick%s(int i) { // pick %s_...\n\tswitch(i) {\n" % (name, name)
@@ -76,9 +70,10 @@ picks = [
   ["ATTRIBUTE_DISPLAY_FLAG","OFF","VALUE","NAME"]]
 
 def makeprint(l):
-  """ Make a print function from a list """
-  name = l[0]
-  s = "string print%s(UL_%s %s) { // print %s_...\n\tswitch(i) {\n" % (name, name)
+  """ Make a print_<type>() function """
+  name = l[0] # name of the type
+  s = "void print_%s(UL_%s %s) { // print %s_...\n\tswitch(i) {\n" %
+      (name, name.upper(), name, name)
   for term in l[1:]:
     s += '\t\tcase %s_%s: return "%s_%s";\n' % (name,term,name,term)
   s += '\t\tdefault: return "%s_unknown";\n\t}\n}\n' % name
@@ -88,4 +83,5 @@ def makeprint(l):
 
 if __name__ == "__main__":
   print header
+  print misc
   for pick in picks: print makepick(pick)
