d9e615359100a0b6871daf5cd0af22f2b101a58d
Michael Yenik <mgyenik@ncsu.edu>
2011-11-04 21:19:28 -0400

    One useless file in up in hurrr.

---
+++ jaustinpage/uvp/yenikclasses.py
@@ -0,0 +1,7 @@
+class componentInstance:
+  def __init__(self, instance_id, library_id):
+    self.instance_id = instance_id
+    self.library_id = library_id
+    self.symbol_index = list()
+  def add_symbol_instance()
+    
