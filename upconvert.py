9aabd36465551d9a7c6fd85e6413e15f329d8f73
Alex Ray <ajray@ncsu.edu>
2011-11-07 03:11:05 -0500

    switch argparse for optparse

--- upconvert.py
+++ upconvert.py
@@ -13,52 +13,55 @@
 
 import os, re, copy, json
 import parser.json, parser.kicad
-import writer.json, writer.kicad
-from optparse import OptionParser
+import parser.eaglexml
+import writer.openjson, writer.kicad
+from argparse import ArgumentParser
 
 parsers = {
     'json': parser.json.JSON,
-    'kicad': parser.kicad.KiCAD
+    'kicad': parser.kicad.KiCAD,
+    'eaglexml': parser.eaglexml.EagleXML
 }
 
 writers = {
-    'json': writer.json.JSON,
+    'openjson': writer.openjson.JSON,
     'kicad': writer.kicad.KiCAD
 }
 
 
-def parse(f, format='kicad'):
+def parse(f, format):
     try:
         p = parsers[format]()
-        return p.parse(f)
     except KeyError:
         print "ERROR: Unsupported input type:", format
         exit(1)
+    return p.parse(f)
 
 
-def write(d, f, format='json'):
+def write(d, f, format='openjson'):
     try:
         w = writers[format]()
-        return w.write(d, f)
     except KeyError:
         print "ERROR: Unsupported output type:", format
         exit(1)
+    return w.write(d, f)
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-i","--input", dest="inputfile",help="read INPUT file in",metavar="INPUT")
-    parser.add_option("-f","--from", dest="inputtype",help="read input file as TYPE",metavar="TYPE")
-    parser.add_option("-o","--output", dest="outputfile",help="write OUTPUT file out",metavar="OUTPUT")
-    parser.add_option("-t","--to", dest="outputtype",help="write output file as TYPE",metavar="TYPE")
+    parser = ArgumentParser()
+    parser.add_argument("-i","--input", dest="inputfile",help="read INPUT file in",metavar="INPUT")
+    parser.add_argument("-f","--from", dest="inputtype",help="read input file as TYPE",metavar="TYPE")
+    parser.add_argument("-o","--output", dest="outputfile",help="write OUTPUT file out",metavar="OUTPUT")
+    parser.add_argument("-t","--to", dest="outputtype",help="write output file as TYPE",metavar="TYPE")
 
-    # parse options
-    (options, args) = parser.parse_args()
-    print "options,args:", options,args
-    inputtype = options.inputtype
-    outputtype = options.outputtype
-    inputfile = options.inputfile
-    outputfile = options.outputfile
+    args = parser.parse_args()
+    inputtype = args.inputtype
+    outputtype = args.outputtype
+    inputfile = args.inputfile
+    outputfile = args.outputfile
+    if None == inputfile:
+      parser.print_help()
+      exit(1)
 
     # parse and export the data
     design = parse(inputfile, inputtype)
